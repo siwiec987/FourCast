71c96900585f6025af7f80ea3a69f13edc8a9189
siwiec987 <siwiec987@gmail.com>
2025-03-07 20:58:21 +0100

    added bottom bar and tabview

--- FourCast Weather/Views/ContentView.swift
+++ FourCast Weather/Views/ContentView.swift
@@ -12,37 +12,81 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     
     var body: some View {
-        ZStack {
-            BackgroundView()
-            
-            ScrollView(showsIndicators: false) {
-                VStack(spacing: 15) {
-                    CurrentWeatherView(weatherData: weatherService.weatherData, locationName: locationManager.locationName)
-                    HourlyForecastView(weatherData: weatherService.weatherData)
-                    DailyForecastView(weatherData: weatherService.weatherData)
-                }
-                .padding()
-                .onChange(of: locationManager.location, initial: false) {
-                    if let location = locationManager.location, locationManager.locationReady {
-                        Task {
-                            do {
-                                try await weatherService.fetchWeatherData(location: location)
-                                
-                            } catch OpenWeatherError.invalidData {
-                                print("Invalid data")
-                            } catch {
-                                print("cos innego wydupcylo")
+        NavigationStack {
+            ZStack {
+                BackgroundView()
+                TabView {
+                    ForEach(0..<5) {_ in
+                        Tab {
+                            ScrollView(showsIndicators: false) {
+                                VStack(spacing: 15) {
+                                    CurrentWeatherView(weatherData: weatherService.weatherData, locationName: locationManager.locationName)
+                                    HourlyForecastView(weatherData: weatherService.weatherData)
+                                    DailyForecastView(weatherData: weatherService.weatherData)
+                                }
+                                .padding()
+                                .onChange(of: locationManager.location, initial: false) {
+                                    if let location = locationManager.location, locationManager.locationReady {
+                                        Task {
+                                            do {
+                                                try await weatherService.fetchWeatherData(location: location)
+                                                
+                                            } catch OpenWeatherError.invalidData {
+                                                print("Invalid data")
+                                            } catch {
+                                                print("cos innego wydupcylo")
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            .refreshable {
+                                locationManager.checkLocationAuthorization()
                             }
                         }
                     }
                 }
-            }
-            .refreshable {
-                locationManager.checkLocationAuthorization()
+                .tabViewStyle(.page)
+                .indexViewStyle(.page(backgroundDisplayMode: .always))
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "plus.square.fill.on.square.fill")
+                                .renderingMode(.original)
+                        }
+                        
+                        Spacer()
+
+                        Text("No internet connection")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundStyle(.red)
+                        
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .renderingMode(.original)
+                        }
+                    }
+                }
+                .toolbarBackground(Material.thin, for: .bottomBar)
+                .toolbarBackgroundVisibility(.visible, for: .bottomBar)
+//                .background(BackgroundView())
+                .tint(.white)
             }
         }
+//        .ignoresSafeArea()
+//            .indexViewStyle(.page(backgroundDisplayMode: .always))
+//        }
     }
 }
+
+
 
 #Preview {
     ContentView()
