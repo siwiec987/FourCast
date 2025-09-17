88731a1b8bd881b5dce30e03c5a29ecbcc173f2c
siwiec987 <siwiec987@gmail.com>
2025-09-17 14:14:03 +0200

    fix + style: correctly show 3-digit temperatures + make it more simple
    Previously there was a bug, when 3-digit temperatures were not shown in single line

--- FourCast Weather/Views/DailyForecastView.swift
+++ FourCast Weather/Views/DailyForecastView.swift
@@ -13,31 +13,37 @@ struct DailyForecastView: View {
     let timezoneOffset: Int
     
     var body: some View {
-        VStack {
-            ForEach(dailyWeatherData, id: \.dt) {dailyForecast in
-                HStack {
+        Grid(verticalSpacing: 10) {
+            ForEach(dailyWeatherData, id: \.dt) { dailyForecast in
+                GridRow {
                     Text("\(Date.getWeekday(from: dailyForecast.dt, with: timezoneOffset))")
-                        .frame(width: 150, alignment: .leading)
+                        .gridColumnAlignment(.leading)
                     
-//                    Spacer()
+                    HStack {
+                        Spacer()
+                        
+                        Image(systemName: WeatherIconMapper.systemIcon(for: dailyForecast.weather[0].icon))
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+
+                        Spacer()
+                    }
+                    .gridColumnAlignment(.center)
                     
-                    Image(systemName: WeatherIconMapper.systemIcon(for: dailyForecast.weather[0].icon))
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
-                    
-                    Spacer()
-                    
-                    Text(UnitFormatter.getFormattedTemperature(dailyForecast.temp.min, to: userSettings.settings.temperatureUnit))
-                        .bold()
-                    Text(". . .")
-                        .bold()
-                    Text(UnitFormatter.getFormattedTemperature(dailyForecast.temp.max, to: userSettings.settings.temperatureUnit))
-                        .bold()
+                    HStack {
+                        Text(UnitFormatter.getFormattedTemperature(dailyForecast.temp.min, to: userSettings.settings.temperatureUnit))
+                            .foregroundStyle(.secondary)
+//                        Text(". . .")
+                        Spacer()
+                        Text(UnitFormatter.getFormattedTemperature(dailyForecast.temp.max, to: userSettings.settings.temperatureUnit))
+                    }
+                    .bold()
+//                    .fixedSize(horizontal: true, vertical: false)
+                    .gridColumnAlignment(.trailing)
                 }
             }
-            .padding(5)
         }
         .weatherComponent()
     }
